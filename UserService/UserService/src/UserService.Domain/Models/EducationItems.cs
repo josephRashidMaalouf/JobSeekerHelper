d247feee96fb6d6368a21da988c814e59b7f2da3
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-12-22 00:44:21 +0100

    Add models

---
+++ UserService/UserService/src/UserService.Domain/Models/EducationItems.cs
@@ -0,0 +1,12 @@
+﻿namespace UserService.Domain.Models;
+
+public class EducationItems
+{
+    public required string SchoolName { get; set; }
+    public required string Program { get; set; }
+    public required string Description { get; set; }
+    public required string Degree { get; set; }
+    public required bool IsOnGoing { get; set; }
+    public required DateOnly StartDate { get; set; }
+    public DateOnly? EndDate { get; set; }
+}
---
+++ UserService/UserService/src/UserService.Domain/Models/WorkItem.cs
@@ -0,0 +1,11 @@
+﻿namespace UserService.Domain.Models;
+
+public class WorkItem
+{
+    public required string CompanyName { get; set; }
+    public required string JobTitle { get; set; }
+    public required string Description { get; set; }
+    public required bool IsOnGoing { get; set; }
+    public required DateOnly StartDate { get; set; }
+    public DateOnly? EndDate { get; set; }
+}
