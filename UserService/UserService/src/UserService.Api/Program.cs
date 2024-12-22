b5750263394785f158e78c0bce1550a2a74ae9c8
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-12-22 23:07:24 +0100

    Register repositories in DI container

--- UserService/UserService/src/UserService.Api/Program.cs
+++ UserService/UserService/src/UserService.Api/Program.cs
@@ -1,41 +1,35 @@
+using UserService.Domain.Entities;
+using UserService.Domain.Interfaces;
+using UserService.Infrastructure.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "";
+var dbName = builder.Configuration.GetSection("Database")["Name"] ?? "";
+
+builder.Services.AddScoped<IResumeRepository, ResumeRepository>(provider =>
+{
+    var collectionName = builder.Configuration.GetSection("Database")["Resumes"] ?? "";
+    return new ResumeRepository(connectionString, dbName, collectionName);
+});
+builder.Services.AddScoped<ISearchSettingsRepository, SearchSettingsRepository>(provider =>
+{
+    var collectionName = builder.Configuration.GetSection("Database")["SearchSettingsCollection"] ?? "";
+    return new SearchSettingsRepository(connectionString, dbName, collectionName);
+});
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
+app.MapOpenApi();
+
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
