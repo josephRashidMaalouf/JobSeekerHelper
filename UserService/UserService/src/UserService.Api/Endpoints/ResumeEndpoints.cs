075aa0e361e3105700ede11a72d5a253cf0c9ae8
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-12-30 19:09:37 +0100

    start writing resume endpoints

---
+++ UserService/UserService/src/UserService.Api/Endpoints/ResumeEndpoints.cs
@@ -0,0 +1,54 @@
+﻿using UserService.Domain.Entities;
+using UserService.Domain.Interfaces;
+
+namespace UserService.Api.Endpoints;
+
+public static class ResumeEndpoints
+{
+    public static IEndpointRouteBuilder MapResumeEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("api/user/{userId:guid}/resume");
+
+        group.MapGet("", GetAllByUserIdAsync);
+        group.MapGet("/{resumeId:guid}", GetByResumeIdAsync);
+
+        return app;
+    }
+
+    private static async Task<IResult> GetAllByUserIdAsync(IResumeService service, Guid userId)
+    {
+        var result = await service.GetAllByUserIdAsync(userId);
+
+        if (!result.IsSuccess)
+        {
+            return result.Code == 404 ? Results.NotFound(result.ErrorMessage) : Results.BadRequest(result.ErrorMessage);
+        }
+        
+        return Results.Ok(result.Data);
+    }
+
+    private static async Task<IResult> GetByResumeIdAsync(IResumeService service, Guid resumeId, Guid userId)
+    {
+        var result = await service.GetByIdAsync(resumeId);
+
+        if (!result.IsSuccess)
+        {
+            return result.Code == 404 ? Results.NotFound(result.ErrorMessage) : Results.BadRequest(result.ErrorMessage);
+        }
+        
+        return Results.Ok(result.Data);
+    }
+
+    private static async Task<IResult> PostAsync(IResumeService service, Guid userId, Resume resume)
+    {
+        var result = await service.AddAsync(resume);
+
+        if (!result.IsSuccess)
+        {
+            return result.Code == 404 ? Results.NotFound(result.ErrorMessage) : Results.BadRequest(result.ErrorMessage);
+        }
+        
+        return Results.Ok(result.Data);
+    }
+    
+}
--- UserService/UserService/src/UserService.Infrastructure/Persistence/Repositories/ResumeRepository.cs
+++ UserService/UserService/src/UserService.Infrastructure/Persistence/Repositories/ResumeRepository.cs
@@ -15,6 +15,13 @@ public class ResumeRepository(string collectionName, string connectionString, st
         try
         {
             var result = await _collection.Find(filter).ToListAsync();
+
+            if (!result.Any())
+            {
+                return Result<List<Resume>>
+                    .Failure($"No resumes found for user with id {userId}", 404);
+            }
+            
             return Result<List<Resume>>.Success(result);
         }
         catch (Exception ex)
