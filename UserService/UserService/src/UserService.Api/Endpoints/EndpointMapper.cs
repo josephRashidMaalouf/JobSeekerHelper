fb758a774bc2e2ded469095fbe8f83c4465b6468
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2025-01-02 14:52:10 +0100

    Add endpoitns to pipeline

---
+++ UserService/UserService/src/UserService.Api/Endpoints/EndpointMapper.cs
@@ -0,0 +1,13 @@
+﻿namespace UserService.Api.Endpoints;
+
+public static class EndpointMapper
+{
+    public static IEndpointRouteBuilder MapEndpoints(this IEndpointRouteBuilder app)
+    {
+
+        app.MapResumeEndpoints();
+        app.MapSearchSettingsEndpoints();
+        
+        return app;
+    }
+}
