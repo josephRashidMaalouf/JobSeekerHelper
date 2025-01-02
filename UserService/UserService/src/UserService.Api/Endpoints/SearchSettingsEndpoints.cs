a5c74f621778e1504a060f4deecebfcb5d0f3d6c
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2025-01-02 14:50:32 +0100

    add resume endpoitns

---
+++ UserService/UserService/src/UserService.Api/Endpoints/SearchSettingsEndpoints.cs
@@ -0,0 +1,78 @@
+﻿using UserService.Domain.Entities;
+using UserService.Domain.Interfaces;
+
+namespace UserService.Api.Endpoints;
+
+public static class SearchSettingsEndpoints
+{
+    public static IEndpointRouteBuilder MapSearchSettingsEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("api/user/{userId:guid}/searchSettings");
+
+        group.MapGet("", GetAllByUserIdAsync);
+        group.MapGet("/{searchSettingsId:guid}", GetBySearchSettingsIdAsync);
+        group.MapPost("", PostAsync);
+        group.MapPut("", UpdateAsync);
+        group.MapDelete("", DeleteAsync);
+
+        return group;
+    }
+
+    private static async Task<IResult> GetAllByUserIdAsync(ISearchSettingsService service, Guid userId)
+    {
+        var result = await service.GetAllByUserIdAsync(userId);
+
+        if (!result.IsSuccess)
+        {
+            return result.Code == 404 ? Results.NotFound(result.ErrorMessage) : Results.BadRequest(result.ErrorMessage);
+        }
+
+        return Results.Ok(result.Data);
+    }
+
+    private static async Task<IResult> GetBySearchSettingsIdAsync(ISearchSettingsService service, Guid searchSettingsId, Guid userId)
+    {
+        var result = await service.GetByIdAsync(searchSettingsId, userId);
+
+        if (!result.IsSuccess)
+        {
+            return result.Code == 404 ? Results.NotFound(result.ErrorMessage) : Results.BadRequest(result.ErrorMessage);
+        }
+
+        return Results.Ok(result.Data);
+    }
+
+    private static async Task<IResult> PostAsync(ISearchSettingsService service, Guid userId, SearchSettings searchSettings)
+    {
+        var result = await service.AddAsync(searchSettings, userId);
+
+        if (!result.IsSuccess)
+        {
+            Results.BadRequest(result.ErrorMessage);
+        }
+
+        return Results.Ok(result.Data);
+    }
+    private static async Task<IResult> UpdateAsync(ISearchSettingsService service, Guid userId, SearchSettings searchSettings)
+    {
+        var result = await service.UpdateAsync(searchSettings, userId);
+
+        if (!result.IsSuccess)
+        {
+            return result.Code == 404 ? Results.NotFound(result.ErrorMessage) : Results.BadRequest(result.ErrorMessage);
+        }
+
+        return Results.Ok(result.Data);
+    }
+    private static async Task<IResult> DeleteAsync(ISearchSettingsService service, Guid userId, Guid searchSettingsId)
+    {
+        var result = await service.DeleteAsync(searchSettingsId, userId);
+
+        if (!result.IsSuccess)
+        {
+            return result.Code == 404 ? Results.NotFound(result.ErrorMessage) : Results.BadRequest(result.ErrorMessage);
+        }
+
+        return Results.Ok(result.Data);
+    }
+}
