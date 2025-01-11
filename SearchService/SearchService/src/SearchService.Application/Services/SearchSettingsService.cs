2b09938e1c14f84b94b79d2b38754b500a864350
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2025-01-11 23:01:13 +0100

    Move application stuff over from UserService

---
+++ SearchService/SearchService/src/SearchService.Application/Services/SearchSettingsService.cs
@@ -0,0 +1,34 @@
+﻿using SearchService.Domain.Entities;
+using SearchService.Domain.Interfaces;
+using SearchService.Domain.Models;
+
+namespace SearchService.Application.Services;
+
+public class SearchSettingsService(ISearchSettingsRepository repo) : ISearchSettingsService
+{
+    private readonly ISearchSettingsRepository _searchSettingsRepository = repo;
+    public async Task<Result<SearchSettings>> GetByIdAsync(Guid id, Guid userId)
+    {
+        return await _searchSettingsRepository.GetByIdAsync(id, userId);
+    }
+
+    public async Task<Result<SearchSettings>> AddAsync(SearchSettings entity, Guid userId)
+    {
+        return await _searchSettingsRepository.AddAsync(entity, userId);
+    }
+
+    public async Task<Result<SearchSettings>> UpdateAsync(SearchSettings entity, Guid userId)
+    {
+        return await _searchSettingsRepository.UpdateAsync(entity, userId);
+    }
+
+    public async Task<Result<Guid>> DeleteAsync(Guid id, Guid userId)
+    {
+        return await _searchSettingsRepository.DeleteAsync(id, userId);
+    }
+
+    public async Task<Result<List<SearchSettings>>> GetAllByUserIdAsync(Guid userId)
+    {
+        return await _searchSettingsRepository.GetAllByUserIdAsync(userId);
+    }
+}
