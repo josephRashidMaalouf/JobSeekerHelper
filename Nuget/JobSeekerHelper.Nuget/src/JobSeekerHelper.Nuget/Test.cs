0acd47624b7e850745bc5627f64e4fa92e2e17b3
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2025-01-15 08:43:07 +0100

    setting up for nuget

---
+++ Nuget/JobSeekerHelper.Nuget/src/JobSeekerHelper.Nuget/Test.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace JobSeekerHelper.Nuget
+{
+    public static class Test
+    {
+        public static void Run()
+        {
+            Debug.WriteLine("JobSeekerHelper.Nuget.Test.Run() called");
+        }
+    }
+}
